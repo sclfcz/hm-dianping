9f2e4c3373c5cd91ef37fe33673c476cf2fb4a30
hepushuo <2775885364@qq.com>
2026-05-21 11:52:54 +0800

    UV统计测试功能实现

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -86,4 +86,19 @@ class HmDianPingApplicationTests {
         }
     }
 
+    @Test
+    void testHyperLogLog() {
+        String[] values = new String[1000];
+        int j;
+        for (int i = 0; i < 1000000; i++) {
+            j = i % 1000;
+            values[j] = "user_" + i;
+            if (j == 999) {
+                stringRedisTemplate.opsForHyperLogLog().add("hl2", values);
+            }
+        }
+        Long size = stringRedisTemplate.opsForHyperLogLog().size("hl2");
+        System.out.println("size = " + size);
+    }
+
 }
